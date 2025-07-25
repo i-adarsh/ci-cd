6020b5077df3ff19b249b39df23904a0ec180b0c
i-adarsh <i.adarsh.kr@icloud.com>
2025-07-25 19:17:40 +0000

    aws test commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,4 @@
-'use client';
-
-import React from 'react';
-import Header from '../components/Header';
-import Hero from '../components/Hero';
-import Features from '../components/Features';
-import About from '../components/About';
-import Contact from '../components/Contact';
-import Footer from '../components/Footer';
-
-
-// --- Main Page Component ---
-export default function Page() {
-  return (
-    <div className="font-sans bg-gradient-to-b from-white to-slate-50 text-slate-800">
-      <Header />
-      <main>
-        <Hero />
-        <Features />
-        <About />
-        <Contact />
-      </main>
-      <Footer />
-    </div>
-  );
-}
+<>
+<h1>Hello from Devops HQ </h1>
+<h3>This is a CI/CD test </h3>
+</>
