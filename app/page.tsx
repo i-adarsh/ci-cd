ac88af4ffa42a1f7f6c58c66d1b5b01dd6080684
i-adarsh <i.adarsh.kr@icloud.com>
2025-07-25 18:25:50 +0000

    updated components

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 'use client';
 
 import React from 'react';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import Features from './components/Features';
-import About from './components/About';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+import Header from '../components/Header';
+import Hero from '../components/Hero';
+import Features from '../components/Features';
+import About from '../components/About';
+import Contact from '../components/Contact';
+import Footer from '../components/Footer';
 
 
 // --- Main Page Component ---
